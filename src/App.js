aab15edcd91acff53200957fde6de495d24ce91e
Sara <sara@carriot.ir>
2022-07-19 02:08:03 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text-container">
+        گوگولی (GOOGLE)
+      </div>
+      <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center"></div>
+        <InputGroup className="col-6">
+          <FormControl id="search-bar"
+            placeholder="جست‌وجو"
+            aria-label="Search"
+            aria-describedby="basic-addon2"
+          /> 
+          
+          
+        </InputGroup>
+        <Button variant="outline-secondary" id="button-addon2">
+            بجو!
+        </Button>
+        
+        <Button variant="outline-secondary" id="button-addon2">
+            بجو!
+        </Button>
+      </div>
     </div>
   );
 }
